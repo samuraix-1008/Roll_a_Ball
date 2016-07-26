c8f4254388b84d99ed243cbe280bd6139a0b5c26
Samantha Raeder <samraeder08@gmail.com>
2016-07-26 15:09:44 -0500

    Finished tutorial

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,7 +5,11 @@ public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
+		int num1 = 5 + 6;
+		float num2 = 3.5f;
+		string word = "Word1" + "Word2"; 
+
 	}
 	
 	// Update is called once per frame
